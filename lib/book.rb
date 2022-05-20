c7674eafb8cf891cdc4b8339441ed6cbbda43ce5
Amer Ibric <ameribric@gmail.com>
2022-05-20 18:29:11 -0400

    commit tree da51d3d2ad1e6096709d65a216d995af17c16596

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,26 @@
+# MY ANWSER
+# class Book
+#     attr_accessor :title, :author_name, :page_count, :genre
+#     def initialize (title, author_name, page_count, genre)
+#         @title = title
+#         @author_name = author_name
+#         @page_count = page_count
+#         @genre = genre
+#     end
+#     def turn_page(title)
+#     end
+# end
 
-
+#SOLUTION
+class Book
+    attr_accessor :author, :page_count, :genre
+    attr_reader :title
+  
+    def initialize(title)
+      @title = title
+    end
+  
+    def turn_page
+      puts "Flipping the page...wow, you read fast!"
+    end
+  end
